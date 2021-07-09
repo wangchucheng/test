80dd7beaeb5347c5d3722bacbabbaedf6c8ef6d2
Wang Chucheng <me@wangchucheng.com>
2021-07-09 22:14:51 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 case "${GITHUB_EVENT_NAME}" in
   release)
     mkdir hugo-eureka-starters-deploy
-    git clone ${INPUT_TARGET_URL} hugo-eureka-starters-deploy
+    git clone https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://} hugo-eureka-starters-deploy
     cd hugo-eureka-starters-deploy
     rm -rf config
     rm -rf content
@@ -11,12 +11,11 @@ case "${GITHUB_EVENT_NAME}" in
     cp -r ../exampleSite/content content
     git config --global user.name ${INPUT_GIT_USERNAME}
     git config --global user.email ${INPUT_GIT_EMAIL}
-    git remote set-url origin https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
     git add .
     git commit -m "refactor: upgrade to hugo eureka ${GITHUB_REF#refs/tags/}"
     git tag ${GITHUB_REF}
     git push --all origin
-    git push --tags origin
+    git push -f --tags origin
     ;;
   *)
     break
