b315fbfbcb6a6a4e069b9df38064f981bdec8c76
Wang Chucheng <me@wangchucheng.com>
2021-07-09 22:48:28 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ case "${GITHUB_EVENT_NAME}" in
     rm -rf content
     cp -r ../exampleSite/config config
     cp -r ../exampleSite/content content
-    sed "s/require github.com\/wangchucheng\/hugo-eureka .* \/\/ indirect/require github.com\/wangchucheng\/hugo-eureka ${GITHUB_REF##*/} \/\/ indirect/g" data/eureka.yaml
+    sed "s/require github.com\/wangchucheng\/hugo-eureka .* \/\/ indirect/require github.com\/wangchucheng\/hugo-eureka ${GITHUB_REF##*/} \/\/ indirect/g" go.mod
     git add .
     git commit -m "refactor: upgrade to hugo eureka ${GITHUB_REF##*/}"
     git tag ${GITHUB_REF}
